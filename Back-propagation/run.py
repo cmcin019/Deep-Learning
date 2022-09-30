Correct gradients

Back-propagation/run.py
@@ -10,7 +10,7 @@ from random import random
 # x is a vector of dim K
 # A, B, C are square mats of dim K x K
 
-def mat_vec(A, x) :
+def mat_vec(A, x):
 	K = len(x)
 	mat=[]
 	for i in range(K):
@@ -19,6 +19,19 @@ def mat_vec(A, x) :
 			result += A[j][i] * x[j]
 		mat.append(result)
 	return mat
+
+def mat_mat(A, B):
+	K = len(A)
+	m = []
+	for i in range(K):
+		c = []
+		for j in range(K):
+			r = 0
+			for k in range(K):
+				r += (A[i][k] * B[k][j])
+			c.append(r)
+		m.append(c)
+	return m
 
 def sigmoid(y):
 	sig = []
@@ -29,27 +42,31 @@ def sigmoid(y):
 def sigmoid_derivation(out):
 	return 1 / (1 + math.exp( -out )) * (1 - 1 / (1 + math.exp( -out )))
 
-def mat_add(u, v):
+def vec_add(u, v):
 	res = []
 	for i in range(len(u)):
 		res.append(u[i] + v[i])
 	return res
 	
-def eucld_dist(w):
+def forward_loss(w):
 	dist = sum([math.pow(i, 2) for i in w])
 	return dist
 	
 def forward(x, A, B, C):
 	net = {}
 	net['x'] = x
+	net['A'] = A
+	net['B'] = B
+	net['C'] = C
 	net['y'] = mat_vec(A, x)
-	net['u'] = mat_vec(B, x)
-	net['v'] = sigmoid(net['y'])
-	net['z'] = mat_add(net['u'], net['v'])
+	net['v'] = mat_vec(B, x)
+	net['u'] = sigmoid(net['y'])
+	net['z'] = vec_add(net['u'], net['v'])
 	net['w'] = mat_vec(C, net['z'])
 	return net
 
-def C_gradient(C, net):
+def C_gradient(net):
+	C = net['C']
 	gradients = []
 	for i in range(len(C)):
 		gradient = []
@@ -58,40 +75,54 @@ def C_gradient(C, net):
 		gradients.append(gradient)
 	return gradients
 
-def B_gradient(B, net, C_g):
+def a_L_minus_1(net):
+	C = net['C']
+	gradients = []
+	for i in range(len(C)):
+		gradient = []
+		for j in range(len(C[i])):
+			# gradient.append(2 * mat_vec(C, net['z'])[j] * net['C'][i][j])
+			gradient.append(2 * net['w'][j] * net['C'][i][j])
+		gradients.append(gradient)
+	return gradients
+
+def B_gradient(net):
+	B = net['B']
 	gradients = []
 	for i in range(len(B)):
 		gradient = []
 		for j in range(len(B[i])):
-			# gradient.append(4 * net['x'][i] * sum(C_g[j]) * (net['u'][j] + net['v'][j]))
-			print(net['x'][i])
-			print(net['u'][i])
-			print(net['z'][i])
-			print(net['w'][i])
-			print(B)
-			print(C_g)
-			gradient.append(2*net['x'][i] * net['w'][j])
-			gradient.append(4*sum(C_g[j]) * net['x'][i] * net['u'][i])
-			# gradient.append(math.prod(C_g[j]) 	* net['x'][i] )
-			# gradient.append(1 					* net['x'][i] )
-			
-			# gradient.append(sum(C_g[i]) 		* net['x'][i] )
-			# gradient.append(math.prod(C_g[i]) 	* net['x'][i] )
-		
+			gradient.append(net['x'][i] * sum(a_L_minus_1(net)[j]))
 		gradients.append(gradient)
 	return gradients
 
-def A_gradient(A, net, C_g):
+def A_gradient(net):
+	A = net['A']
 	gradients = []
 	for i in range(len(A)):
 		gradient = []
 		for j in range(len(A[i])):
-			gradient.append(net['x'][i] * sigmoid_derivation(net['y'][i]) * math.prod(C_g[j]))
+			gradient.append(net['x'][i] * sigmoid_derivation(net['y'][j]) * sum(a_L_minus_1(net)[j]))
 		gradients.append(gradient)
 	return gradients
 
-def backward(x, A, B, C):
-	pass
+def backward(f):
+	C_g = C_gradient(f)
+	B_g = B_gradient(f)
+	A_g = A_gradient(f)
+	return A_g, B_g, C_g
+
+def gradient_descent(lr, N, net):
+	K = len(net['x'])
+	for _ in range(N):
+		net = forward([random() for _ in range(K)], net['A'], net['B'], net['C'])
+		A_g, B_g, C_g = backward(net)
+		for i in range(K):
+			for j in range(K):
+				net['A'][i][j] = net['A'][i][j] - lr * A_g[i][j] / N
+				net['B'][i][j] = net['B'][i][j] - lr * B_g[i][j] / N
+				net['C'][i][j] = net['C'][i][j] - lr * C_g[i][j] / N
+	return net
 	
 import torch
 def test(x, A, B, C):
@@ -102,13 +133,9 @@ def test(x, A, B, C):
 
 	net_forward = torch.matmul((torch.sigmoid(torch.matmul(t_x, t_A)) + torch.matmul(t_x, t_B)), t_C)
 
-	# external_grad = torch.tensor([1., 1.])
-	# net_forward.backward()
-
-	# loss = torch.nn.functional.mse_loss(net_forward, t_x)
 	loss = torch.sum(torch.pow(net_forward, 2))
+	print("Torch loss", loss)
 	loss.backward()
-
 	print("REAL:")
 	print(f"{t_C.grad}")
 	print()
@@ -116,28 +143,11 @@ def test(x, A, B, C):
 	print()
 	print(f"{t_A.grad}")
 	print()
-
-def run():
-
-	K = 1
-	A = [[random() for _ in range(K)] for _ in range(K)]
-	B = [[random() for _ in range(K)] for _ in range(K)]
-	C = [[random() for _ in range(K)] for _ in range(K)]
-
-	x = [random() for _ in range(K)]
-
 	f = forward(x, A, B, C)
-
-	C_g = C_gradient(C, f)
-	B_g = B_gradient(C,f,C_g)
-	A_g = A_gradient(A,f,C_g)
-
-	test(x, A, B, C)
-
-
-	# print(f)
-	# print()
-	print(A,C)
+	A_g, B_g, C_g = backward(f)
+	print()
+	loss = forward_loss(f['w'])
+	print("My loss", loss)
 	print("MINE:")
 	print(torch.tensor(C_g))
 	print()
@@ -146,29 +156,30 @@ def run():
 	print(torch.tensor(A_g))
 
 
-	# gradient = []
-	# i=0
-	# for j in range(len(B[i])):
-	# 	gradient.append(f['x'][i] * sum(C_g[j]))
-	# 	print(gradient[-1])
-	# 	gradient.append(f['x'][j] * sum(C_g[i]))
-	# 	print(gradient[-1])
-	# 	gradient.append(f['x'][i] * math.prod(C_g[j]))
-	# 	print(gradient[-1])
-	# 	gradient.append(f['x'][j] * math.prod(C_g[i]))
-	# 	print(gradient[-1])
+def run():
 
+	K = 5
+	A = [[random() for _ in range(K)] for _ in range(K)]
+	B = [[random() for _ in range(K)] for _ in range(K)]
+	C = [[random() for _ in range(K)] for _ in range(K)]
 
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
-	# 	gradient.append(f['x'][i] * sum(f['x']) * sum(C_g[j]))
+	x = [random() for _ in range(K)]
 
+	f = forward(x, A, B, C)
+	# A_g, B_g, C_g = backward(f)
+
+	loss = forward_loss(f['w'])
+	print("Loss Before GD:")
+	print(loss)
+	f = gradient_descent(0.05, 1000, f)
+	loss = forward_loss(f['w'])
+	print("Loss After GD:")
+	print(loss)
+	
+	# TEST #
+	###############
+	# test(x, A, B, C)
+	##############
 
 
 