6ffac5884a3f660a2d977bc78fbacf19564ce93e
cmcin019 <25cristopher25@gmail.com>
2022-10-05 18:50:10 -0400

    Change documentation

--- MNIST-Classification/run.py
+++ MNIST-Classification/run.py
@@ -18,11 +18,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.utils.data import DataLoader
-import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms 
 
-from torch.autograd import Variable
 torch.cuda.is_available()
 
 # Set device
@@ -73,6 +71,8 @@ class MLP(nn.Module):
 			a = F.relu(self.hidden(a))
 		if self.drop:
 			a = self.dropout(a)
+		if self.bn:
+			a = self.batchnorm(a)
 		a = self.final(a)
 		a = self.soft_max(a)
 		return a
@@ -83,12 +83,10 @@ class CNN(nn.Module):
 		l = 28 - (k//2)*2*conv_layers
 		self.l = l
 		self.name = f'CNN - k={k} layers={conv_layers} dropout={drop} bn={bn}'
-		# print([(16 // math.pow(2, (conv_layers)), 16 // math.pow(2, (conv_layers-x-2)), k) for x in range(conv_layers-1)])
 		self.convs = nn.ModuleList(
 			[
 				nn.Conv2d(1, int(16 // math.pow(2, (conv_layers-1))), k)
-			] +
-			[
+			] + [
 				nn.Conv2d(int(16 // math.pow(2, int(conv_layers-x-1))), int(16 // math.pow(2, (conv_layers-x-2))), k) for x in range(conv_layers-1)
 			]
 		)
@@ -105,7 +103,7 @@ class CNN(nn.Module):
 		a = image.view(-1, 16 * self.l * self.l)
 		if self.drop:
 			a = self.dropout(a)
-		if self.drop:
+		if self.bn:
 			a = self.batchnorm(a)
 		a = self.fc(a)
 		a = self.soft_max(a)
@@ -135,98 +133,100 @@ test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=True)
 
 
 # Intantiate models
-sm_r = SoftMax_Regression().to(device=device)
+# Simple configurations
+sm_r = SoftMax_Regression()
+mlp_28_2 = MLP(width=28, depth=2)
+cnn_3_2 = CNN(k=3, conv_layers=2)
 
-mlp_28_2 = MLP(width=28, depth=2).to(device=device)
-cnn_3_2 = CNN(k=3, conv_layers=2).to(device=device)
 
+# Model variations
+mlp_28_1 = MLP(width=28, depth=1)
+cnn_3_3 = CNN(k=3, conv_layers=3)
 
-mlp_28_1 = MLP(width=28, depth=1).to(device=device)
-cnn_3_3 = CNN(k=3, conv_layers=3).to(device=device)
+mlp_56_1 = MLP(width=56, depth=1)
+cnn_5_3 = CNN(k=5, conv_layers=3)
 
-mlp_56_1 = MLP(width=56, depth=1).to(device=device)
-cnn_5_3 = CNN(k=5, conv_layers=3).to(device=device)
+mlp_56_3 = MLP(width=56, depth=3)
+cnn_5_1 = CNN(k=5, conv_layers=1)
 
-mlp_56_3 = MLP(width=56, depth=3).to(device=device)
-cnn_5_1 = CNN(k=5, conv_layers=1).to(device=device)
+mlp_28_3 = MLP(width=28, depth=3)
+cnn_3_1 = CNN(k=3, conv_layers=1)
 
-mlp_28_3 = MLP(width=28, depth=3).to(device=device)
-cnn_3_1 = CNN(k=3, conv_layers=1).to(device=device)
+mlp_112_2 = MLP(width=112, depth=2)
+cnn_7_1 = CNN(k=7, conv_layers=1)
 
-mlp_112_2 = MLP(width=112, depth=2).to(device=device)
-cnn_7_1 = CNN(k=7, conv_layers=1).to(device=device)
-
-mlp_112_1 = MLP(width=112, depth=1).to(device=device)
-cnn_7_3 = CNN(k=7, conv_layers=3).to(device=device)
+mlp_112_1 = MLP(width=112, depth=1)
+cnn_7_3 = CNN(k=7, conv_layers=3)
 
 
 # Models with dropout
-sm_r_dr = SoftMax_Regression(drop=True).to(device=device)
+sm_r_dr = SoftMax_Regression(drop=True)
 
-mlp_28_1_dr = MLP(width=28, depth=1, drop=True).to(device=device)
-cnn_3_3_dr = CNN(k=3, conv_layers=3, drop=True).to(device=device)
+mlp_28_1_dr = MLP(width=28, depth=1, drop=True)
+cnn_3_3_dr = CNN(k=3, conv_layers=3, drop=True)
 
-mlp_56_1_dr = MLP(width=56, depth=1, drop=True).to(device=device)
-cnn_5_3_dr = CNN(k=5, conv_layers=3, drop=True).to(device=device)
+mlp_56_1_dr = MLP(width=56, depth=1, drop=True)
+cnn_5_3_dr = CNN(k=5, conv_layers=3, drop=True)
 
-mlp_56_3_dr = MLP(width=56, depth=3, drop=True).to(device=device)
-cnn_5_1_dr = CNN(k=5, conv_layers=1, drop=True).to(device=device)
+mlp_56_3_dr = MLP(width=56, depth=3, drop=True)
+cnn_5_1_dr = CNN(k=5, conv_layers=1, drop=True)
 
-mlp_28_3_dr = MLP(width=28, depth=3, drop=True).to(device=device)
-cnn_3_1_dr = CNN(k=3, conv_layers=1, drop=True).to(device=device)
+mlp_28_3_dr = MLP(width=28, depth=3, drop=True)
+cnn_3_1_dr = CNN(k=3, conv_layers=1, drop=True)
 
-mlp_112_2_dr = MLP(width=112, depth=2, drop=True).to(device=device)
-cnn_7_1_dr = CNN(k=7, conv_layers=1, drop=True).to(device=device)
+mlp_112_2_dr = MLP(width=112, depth=2, drop=True)
+cnn_7_1_dr = CNN(k=7, conv_layers=1, drop=True)
 
-mlp_112_1_dr = MLP(width=112, depth=1, drop=True).to(device=device)
-cnn_7_3_dr = CNN(k=7, conv_layers=3, drop=True).to(device=device)
+mlp_112_1_dr = MLP(width=112, depth=1, drop=True)
+cnn_7_3_dr = CNN(k=7, conv_layers=3, drop=True)
 
 
 # Models with batch normalization
-sm_r_bn = SoftMax_Regression(bn=True).to(device=device)
+sm_r_bn = SoftMax_Regression(bn=True)
 
-mlp_28_1_bn = MLP(width=28, depth=1, bn=True).to(device=device)
-cnn_3_3_bn = CNN(k=3, conv_layers=3, bn=True).to(device=device)
+mlp_28_1_bn = MLP(width=28, depth=1, bn=True)
+cnn_3_3_bn = CNN(k=3, conv_layers=3, bn=True)
 
-mlp_56_1_bn = MLP(width=56, depth=1, bn=True).to(device=device)
-cnn_5_3_bn = CNN(k=5, conv_layers=3, bn=True).to(device=device)
+mlp_56_1_bn = MLP(width=56, depth=1, bn=True)
+cnn_5_3_bn = CNN(k=5, conv_layers=3, bn=True)
 
-mlp_56_3_bn = MLP(width=56, depth=3, bn=True).to(device=device)
-cnn_5_1_bn = CNN(k=5, conv_layers=1, bn=True).to(device=device)
+mlp_56_3_bn = MLP(width=56, depth=3, bn=True)
+cnn_5_1_bn = CNN(k=5, conv_layers=1, bn=True)
 
-mlp_28_3_bn = MLP(width=28, depth=3, bn=True).to(device=device)
-cnn_3_1_bn = CNN(k=3, conv_layers=1, bn=True).to(device=device)
+mlp_28_3_bn = MLP(width=28, depth=3, bn=True)
+cnn_3_1_bn = CNN(k=3, conv_layers=1, bn=True)
 
-mlp_112_2_bn = MLP(width=112, depth=2, bn=True).to(device=device)
-cnn_7_1_bn = CNN(k=7, conv_layers=1, bn=True).to(device=device)
+mlp_112_2_bn = MLP(width=112, depth=2, bn=True)
+cnn_7_1_bn = CNN(k=7, conv_layers=1, bn=True)
 
-mlp_112_1_bn = MLP(width=112, depth=1, bn=True).to(device=device)
-cnn_7_3_bn = CNN(k=7, conv_layers=3, bn=True).to(device=device)
+mlp_112_1_bn = MLP(width=112, depth=1, bn=True)
+cnn_7_3_bn = CNN(k=7, conv_layers=3, bn=True)
 
 
 # Models with dropout & batch normalization
-sm_r_dr_bn = SoftMax_Regression(drop=True, bn=True).to(device=device)
+sm_r_dr_bn = SoftMax_Regression(drop=True, bn=True)
 
-mlp_28_1_dr_bn = MLP(width=28, depth=1, drop=True, bn=True).to(device=device)
-cnn_3_3_dr_bn = CNN(k=3, conv_layers=3, drop=True, bn=True).to(device=device)
+mlp_28_1_dr_bn = MLP(width=28, depth=1, drop=True, bn=True)
+cnn_3_3_dr_bn = CNN(k=3, conv_layers=3, drop=True, bn=True)
 
-mlp_56_1_dr_bn = MLP(width=56, depth=1, drop=True, bn=True).to(device=device)
-cnn_5_3_dr_bn = CNN(k=5, conv_layers=3, drop=True, bn=True).to(device=device)
+mlp_56_1_dr_bn = MLP(width=56, depth=1, drop=True, bn=True)
+cnn_5_3_dr_bn = CNN(k=5, conv_layers=3, drop=True, bn=True)
 
-mlp_56_3_dr_bn = MLP(width=56, depth=3, drop=True, bn=True).to(device=device)
-cnn_5_1_dr_bn = CNN(k=5, conv_layers=1, drop=True, bn=True).to(device=device)
+mlp_56_3_dr_bn = MLP(width=56, depth=3, drop=True, bn=True)
+cnn_5_1_dr_bn = CNN(k=5, conv_layers=1, drop=True, bn=True)
 
-mlp_28_3_dr_bn = MLP(width=28, depth=3, drop=True, bn=True).to(device=device)
-cnn_3_1_dr_bn = CNN(k=3, conv_layers=1, drop=True, bn=True).to(device=device)
+mlp_28_3_dr_bn = MLP(width=28, depth=3, drop=True, bn=True)
+cnn_3_1_dr_bn = CNN(k=3, conv_layers=1, drop=True, bn=True)
 
-mlp_112_2_dr_bn = MLP(width=112, depth=2, drop=True, bn=True).to(device=device)
-cnn_7_1_dr_bn = CNN(k=7, conv_layers=1, drop=True, bn=True).to(device=device)
+mlp_112_2_dr_bn = MLP(width=112, depth=2, drop=True, bn=True)
+cnn_7_1_dr_bn = CNN(k=7, conv_layers=1, drop=True, bn=True)
 
-mlp_112_1_dr_bn = MLP(width=112, depth=1, drop=True, bn=True).to(device=device)
-cnn_7_3_dr_bn = CNN(k=7, conv_layers=3, drop=True, bn=True).to(device=device)
+mlp_112_1_dr_bn = MLP(width=112, depth=1, drop=True, bn=True)
+cnn_7_3_dr_bn = CNN(k=7, conv_layers=3, drop=True, bn=True)
 
 
 def train(model):
+	model.to(device=device)
 	acc_list = []
 	# Loss and optimizer
 	criterion = nn.CrossEntropyLoss()
@@ -252,6 +252,8 @@ def train(model):
 		acc_list.append(accuracy)
 	
 	print(f'Final accuracy: {accuracy}')
+	if device=='cuda:0':
+		model.to(device='cpu')
 	return acc_list
 
 def model_accuracy(model):
@@ -278,68 +280,80 @@ def model_accuracy(model):
 
 def main() -> None :
 	model_list = [
-		# sm_r, # Basic
+		# # sm_r, # Basic
 		# mlp_28_2, # Basic
-		cnn_3_2, # Basic
+		# # cnn_3_2, # Basic
 		# mlp_28_1,
-		cnn_3_3,
-		# mlp_56_1,
-		cnn_5_3,
-		# mlp_56_3,
-		cnn_5_1,
+		# # cnn_3_1,
 		# mlp_28_3,
-		cnn_3_1,
+		# # cnn_3_3,
+		# mlp_56_1,
+		# # cnn_5_3,
+		# mlp_56_3,
+		# # cnn_5_1,
 		# mlp_112_2,
-		# cnn_7_1,
+		# # cnn_7_1,
+		# mlp_112_1,
+		# # cnn_7_3,
 
-	# 	sm_r_dr,
-	# 	mlp_28_1_dr,
-	# 	cnn_3_3_dr,
-	# 	# mlp_56_1_dr,
-	# 	# cnn_5_3_dr,
-	# 	# mlp_56_3_dr,
-	# 	# cnn_5_1_dr,
-	# 	# mlp_28_3_dr,
-	# 	# cnn_3_1_dr,
+		# sm_r_dr,
+		# mlp_28_1_dr,
+		# # cnn_3_3_dr,
+		# mlp_56_1_dr,
+		# # cnn_5_3_dr,
+		# mlp_56_3_dr,
+		# # cnn_5_1_dr,
+		# mlp_28_3_dr,
+		# # cnn_3_1_dr,
+		# mlp_112_2_dr,
+		# # cnn_7_1_dr,
+		# mlp_112_1_dr,
+		# # cnn_7_3_dr,
 
-	# 	sm_r_bn,
-	# 	mlp_28_1_bn,
-	# 	cnn_3_3_bn,
-	# 	# mlp_56_1_bn,
-	# 	# cnn_5_3_bn,
-	# 	# mlp_56_3_bn,
-	# 	# cnn_5_1_bn,
-	# 	# mlp_28_3_bn,
-	# 	# cnn_3_1_bn,
+		# # sm_r_bn,
+		# mlp_28_1_bn,
+		# # cnn_3_3_bn,
+		# mlp_56_1_bn,
+		# # cnn_5_3_bn,
+		# mlp_56_3_bn,
+		# # cnn_5_1_bn,
+		# mlp_28_3_bn,
+		# # cnn_3_1_bn,
+		# mlp_112_2_bn,
+		# # cnn_7_1_bn,
+		# mlp_112_1_bn,
+		# # cnn_7_3_bn,
 
-	# 	sm_r_dr_bn,
-	# 	mlp_28_1_dr_bn,
-	# 	cnn_3_3_dr_bn,
-	# 	# mlp_56_1_dr_bn,
-	# 	# cnn_5_3_dr_bn,
-	# 	# mlp_56_3_dr_bn,
-	# 	# cnn_5_1_dr_bn,
-	# 	# mlp_28_3_dr_bn,
-	# 	# cnn_3_1_dr_bn,
+		# # sm_r_dr_bn,
+		# mlp_28_1_dr_bn,
+		# # cnn_3_3_dr_bn,
+		# mlp_56_1_dr_bn,
+		# # cnn_5_3_dr_bn,
+		# mlp_56_3_dr_bn,
+		# # cnn_5_1_dr_bn,
+		# mlp_28_3_dr_bn,
+		# # cnn_3_1_dr_bn,
+		# mlp_112_2_dr_bn,
+		# # cnn_7_1_dr_bn,
+		# mlp_112_1_dr_bn,
+		# # cnn_7_3_dr_bn,
+
+		# # Single Model DR & BN Tests 
+		# # sm_r,
+		# # sm_r_dr,
+		# sm_r_bn,
+		# sm_r_dr_bn,
+
+		# mlp_112_2,
+		# mlp_112_2_dr,
+		# mlp_112_2_bn,
+		# mlp_112_2_dr_bn,
+
+		# cnn_5_1,
+		# cnn_5_1_dr,
+		# cnn_5_1_bn,
+		# cnn_5_1_dr_bn
 	]
-
-	# model_list = [
-	# 	sm_r,
-	# 	sm_r_dr,
-	# 	sm_r_bn,
-	# 	sm_r_dr_bn,
-	# 	mlp_28_1,
-	# 	mlp_28_1_dr,
-	# 	mlp_28_1_bn,
-	# 	mlp_28_1_dr_bn,
-	# 	cnn_3_3,
-	# 	cnn_3_3_dr,
-	# 	cnn_3_3_bn,
-	# 	cnn_3_3_dr_bn
-	# ]
-
-
-
 
 	list = [train(m) for m in model_list]
 
