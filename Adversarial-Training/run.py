eaf17391bd4fc9fe196419d4c8cc977eea846c93
cmcin019 <25cristopher25@gmail.com>
2022-10-24 13:23:02 -0400

    Add report draft

--- Adversarial-Training/run.py
+++ Adversarial-Training/run.py
@@ -4,9 +4,7 @@
 
 # Imports
 import argparse
-from glob import glob
 import math
-from operator import mod
 import matplotlib.pyplot as plt
 import numpy as np
 from random import uniform
@@ -65,11 +63,11 @@ class CNN(nn.Module):
 
 # Training Hyperparameters
 learning_rate = 0.001
-num_epochs = 15
+num_epochs = 20
 
 # PGD Hyperparameters
 epsilon=0.3
-steps=20
+steps=5
 alpha=0.02
 targets={0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, 9:0}
 
@@ -122,11 +120,12 @@ def train(model):
 		model.to(device='cpu')
 	return acc_list
 
-def model_accuracy(model, pgd=None, pgd_per=[], plot=False, target_acc=False):
+def model_accuracy(model, pgd=None, pgd_per=[0,0,0], plot=False, target_acc=False, on_training=False):
 	correct = 0
 	total = 0
+	loader = train_loader if on_training else test_loader
 	model.eval()
-	for images, labels in test_loader:
+	for images, labels in loader:
 		images = images.to(device=device)
 		images_saved = images
 		labels = labels.to(device=device)
@@ -142,11 +141,11 @@ def model_accuracy(model, pgd=None, pgd_per=[], plot=False, target_acc=False):
 				top_2 = torch.topk(out.data, 2).indices.tolist()
 				labels = torch.tensor(list(map(lambda x, y: max(x) if min(x) == y else min(x), top_2, labels.data.tolist()))).to(device=device)
 			if plot:
-				system('cls' if os.name == 'nt' else 'clear')
-				print([test_dataset.classes[i][0] for i in predicted])
+
 				np_images = torchvision.utils.make_grid(images.cpu().data, normalize=True).numpy()
-				plt.imshow(np.transpose(np_images,(1,2,0)))
-				plt.show()
+				fig, ax = plt.subplots()
+				ax.imshow(np.transpose(np_images,(1,2,0)))
+				fig.savefig(f'images/{pgd.__name__ if not pgd == None else None} - ϵ: {pgd_per[0]} - steps: {pgd_per[1]} - α: {pgd_per[2]}.jpg', bbox_inches='tight', dpi=150)
 				plot = False
 
 			total += labels.size(0)
@@ -222,7 +221,13 @@ def PGD_Targeted(images, labels, model, epsilon=epsilon, steps=steps, alpha=alph
 def train_with_PGD(model, pgd, pgd_per=[]):
 	model.to(device=device)
 	acc_list = []
+	acc_list_p = []
+	acc_list_np =[]
 	loss_list = []
+	loss_list_np = []
+	loss_list_iter = []
+	loss_list_np_iter = []
+
 	global show_graph
 
 	# Loss and optimizer
@@ -244,32 +249,55 @@ def train_with_PGD(model, pgd, pgd_per=[]):
 
 			scores = model(perturbed)
 			loss = criterion(scores, targets)
-			loss_list.append(float(loss.data))
+			if show_graph:
+				scores_np = model(data)
+				loss_np = criterion(scores_np, targets)	
+				loss_list_iter.append(float(loss.data))
+				loss_list_np_iter.append(float(loss_np.data))
 
 			optimizer.zero_grad()
 			loss.backward(retain_graph=True)
-
 			optimizer.step()
+
+
+		if show_graph:
+			loss_list.append(float(loss.data))
+			loss_list_np.append(float(loss_np.data))
+			accuracy_np = model_accuracy(model, on_training=True)
+			acc_list_np.append(accuracy_np)
+			accuracy_p = model_accuracy(model, pgd=pgd, pgd_per=pgd_per, on_training=True)
+			acc_list_p.append(accuracy_p)
+
 		accuracy = model_accuracy(model)
 		acc_list.append(accuracy)
 
 	# TODO: Need to add observable plots
 	if show_graph:
-		_, ax = plt.subplots()
-		ax.set_title('Accuracy')
-		ax.plot([n for n in range(num_epochs)], acc_list, label=f'{pgd.__name__} - {pgd_per}')
-		ax.set(xlabel='Epoch', ylabel='Accuracy')
-		plt.legend()
+		global ax_unperturbed_training
+		global ax_unperturbed_training_loss
+		global ax_unperturbed_training_loss_iter
+		global ax_perturbed_training
+		global ax_perturbed_training_loss
+		global ax_perturbed_training_loss_iter
 
-		_, ax = plt.subplots()
-		ax.set_title('Loss')
-		ax.plot([n for n in range(len(loss_list))], loss_list, label=f'{pgd.__name__} - {pgd_per}')
-		ax.set(xlabel='Iteration', ylabel='Loss')
-		plt.legend()
+		ax_unperturbed_training.plot([n for n in range(len(acc_list_np))], acc_list_np, label=f'{pgd.__name__} - ϵ: {pgd_per[0]} - steps: {pgd_per[1]} - α: {pgd_per[2]}')
+		ax_unperturbed_training.legend()
 
-		# plt.show()
+		ax_unperturbed_training_loss.plot([n for n in range(len(loss_list_np))], loss_list_np, label=f'{pgd.__name__} - ϵ: {pgd_per[0]} - steps: {pgd_per[1]} - α: {pgd_per[2]}')
+		ax_unperturbed_training_loss.legend()
 
-	# print(f'Final accuracy: {accuracy}')
+		ax_unperturbed_training_loss_iter.plot([n for n in range(len(loss_list_np_iter))], loss_list_np_iter, label=f'{pgd.__name__} - ϵ: {pgd_per[0]} - steps: {pgd_per[1]} - α: {pgd_per[2]}')
+		ax_unperturbed_training_loss_iter.legend()
+
+		ax_perturbed_training.plot([n for n in range(len(acc_list_p))], acc_list_p, label=f'{pgd.__name__} - ϵ: {pgd_per[0]} - steps: {pgd_per[1]} - α: {pgd_per[2]}')
+		ax_perturbed_training.legend()
+
+		ax_perturbed_training_loss.plot([n for n in range(len(loss_list))], loss_list, label=f'{pgd.__name__} - ϵ: {pgd_per[0]} - steps: {pgd_per[1]} - α: {pgd_per[2]}')
+		ax_perturbed_training_loss.legend()
+
+		ax_perturbed_training_loss_iter.plot([n for n in range(len(loss_list_iter))], loss_list_iter, label=f'{pgd.__name__} - ϵ: {pgd_per[0]} - steps: {pgd_per[1]} - α: {pgd_per[2]}')
+		ax_perturbed_training_loss_iter.legend()
+
 	if device=='cuda:0':
 		model.to(device='cpu')
 
@@ -286,7 +314,6 @@ def experiment(original_model, model, pgd, plot=False, pgd_per=[]):
 def run(model, plot, spgd, pgds=[PGD, PGD_Targeted], epsilons=[0, .1, .2, .3, .45], steps=1, alpha=0.01):
 	original_model = CNN(k=5, conv_layers=3, bn=True).to(device=device)
 	original_model.load_state_dict(torch.load('models/model.pt'))
-	acc_original = model_accuracy(model)
 	acc = []
 	for alg in pgds:
 		for eps in epsilons:
@@ -298,20 +325,8 @@ def run(model, plot, spgd, pgds=[PGD, PGD_Targeted], epsilons=[0, .1, .2, .3, .4
 				model.load_state_dict(torch.load('models/model.pt'))
 			pgd_per = [eps,steps,alpha]
 			acc.append((alg.__name__, [eps,steps,alpha], *experiment(original_model, model, alg, plot, pgd_per)))
-	
-	system('cls' if os.name == 'nt' else 'clear')
 
-	print('Pretrained Model' if not spgd else "From Scratch Model")
-	print(f'Accuracy on Original data: {acc_original}')
-	print()
-	for a in acc:
-		print(f'Algorithm: {a[0]}')
-		print(f'Ep: {a[1][0]} - Stps: {a[1][1]} - Alpha: {a[1][2]}')
-		print(f'Model Acc on Perturbed data: {a[2]}')
-		if a[0] == 'PGD_Targeted':
-			print(f'Attack Acc :  {a[3]}')
-		print(f'Acc After Training:  {a[4]}')
-		print()
+	return acc, model
 
 def main() -> None :
 	# Arguments and values 
@@ -340,25 +355,129 @@ def main() -> None :
 		model.load_state_dict(torch.load('models/model.pt'))
 		model.to(device=device)
 
+	acc_original = model_accuracy(model)
+	runs = []
+	models = []
+
+	if show_graph:
+		global ax_unperturbed_training
+		global ax_unperturbed_training_loss
+		global ax_unperturbed_training_loss_iter
+		global ax_perturbed_training
+		global ax_perturbed_training_loss
+		global ax_perturbed_training_loss_iter
+
+		fig_1, ax_unperturbed_training = plt.subplots()
+		ax_unperturbed_training.set_title('Accuracy on Unperturbed Training Data')
+
+		fig_2, ax_unperturbed_training_loss = plt.subplots()
+		ax_unperturbed_training_loss.set_title('Loss on Unperturbed Training Data')
+
+		fig_2_iter, ax_unperturbed_training_loss_iter = plt.subplots()
+		ax_unperturbed_training_loss_iter.set_title('Loss on Unperturbed Training Data')
+
+		fig_3, ax_perturbed_training = plt.subplots()
+		ax_perturbed_training.set_title('Accuracy on Perturbed Training Data')
+
+		fig_4, ax_perturbed_training_loss = plt.subplots()
+		ax_perturbed_training_loss.set_title('Loss on Perturbed Training Data')
+
+		fig_4_iter, ax_perturbed_training_loss_iter = plt.subplots()
+		ax_perturbed_training_loss_iter.set_title('Loss on Perturbed Training Data')
+
 	# TODO: Non-targeted 20-step PGD - ϵ: 0.3 - α: 0.02
-	if args.task == 1 or args.task == -1:
-		run(model, plot=plot, spgd=spgd, pgds=[PGD], epsilons=[.3], steps=20, alpha=.02)
+	if args.task == 1 or args.task == 4:
+		run1, m = run(model, plot=plot, spgd=spgd, pgds=[PGD], epsilons=[.3], steps=20, alpha=.02)
+		runs.append(run1)
+		models.append(m)
 
 	# TODO: Non-targeted 01-step PGD - ϵ: 0.3 - α: 0.5
-	if args.task == 2 or args.task == -1:
-		run(model, plot=plot, spgd=spgd, pgds=[PGD], epsilons=[.3], steps=1, alpha=.5)
+	if args.task == 2 or args.task == 4:
+		run2, m = run(model, plot=plot, spgd=spgd, pgds=[PGD], epsilons=[.3], steps=1, alpha=.5)
+		runs.append(run2)
+		models.append(m)
 
 	# TODO: Targeted 20-step PGD 	 - ϵ: 0.3 - α: 0.02
-	if args.task == 3 or args.task == -1:
-		run(model, plot=plot, spgd=spgd, pgds=[PGD_Targeted], epsilons=[.3], steps=20, alpha=.02)
+	if args.task == 3 or args.task == 4:
+		run3, m = run(model, plot=plot, spgd=spgd, pgds=[PGD_Targeted], epsilons=[.3], steps=20, alpha=.02)
+		runs.append(run3)
+		models.append(m)
+
+	if show_graph:
+		ax_unperturbed_training.set(xlabel='Epoch', ylabel='Accuracy')
+		ax_unperturbed_training_loss.set(xlabel='Epoch', ylabel='Loss')
+		ax_unperturbed_training_loss_iter.set(xlabel='Iteration', ylabel='Loss')
+		ax_perturbed_training.set(xlabel='Epoch', ylabel='Accuracy')
+		ax_perturbed_training_loss.set(xlabel='Epoch', ylabel='Loss')
+		ax_perturbed_training_loss_iter.set(xlabel='Iteration', ylabel='Loss')
+		# plt.legend()
+
+		fig_1.savefig(f'images/Accuracy on Unperturbed Training Data.jpg', bbox_inches='tight', dpi=150)
+
+		fig_2.savefig(f'images/Loss on Unperturbed Training Data.jpg', bbox_inches='tight', dpi=150)
+
+		fig_2_iter.savefig(f'images/Loss on Unperturbed Training Data (iter).jpg', bbox_inches='tight', dpi=150)
+
+		fig_3.savefig(f'images/Accuracy on Perturbed Training Data.jpg', bbox_inches='tight', dpi=150)
+
+		fig_4.savefig(f'images/Loss on Perturbed Training Data.jpg', bbox_inches='tight', dpi=150)
+		
+		fig_4_iter.savefig(f'images/Loss on Perturbed Training Data (iter).jpg', bbox_inches='tight', dpi=150)
 
 	# TODO: Training Evaluation
-	if args.task == 4 or args.task == -1:
-		run(model, plot=plot, spgd=spgd)
-	if show_graph:
-		plt.show()
+	system('cls' if os.name == 'nt' else 'clear')
+	print('Pretrained Model' if not spgd else "From Scratch Model")
+	print(f'Accuracy on Original data: {acc_original}')
+	print()
+
+	plt.close()
+	
+	fig, ax = plt.subplots()
+	ax.set_title('Accuracy on Perturbed Testing Data')
+
+	for r in runs:
+		print()
+		for a in r:
+			ax.plot([n for n in range(len(a[4]))], a[4], label=f'{a[0]} - ϵ: {a[1][0]} - steps: {a[1][1]} - α: {a[1][2]}')
+
+			print(f'Algorithm: {a[0]}')
+			print(f'Ep: {a[1][0]} - Stps: {a[1][1]} - Alpha: {a[1][2]}')
+			print(f'Model Acc on Perturbed data: {a[2]}')
+			if a[0] == 'PGD_Targeted':
+				print(f'Attack Acc :  {a[3]}')
+			print(f'Acc After Training:  {a[4]}')
+			print()
+	
+	ax.set(xlabel='Epoch', ylabel='Accuracy')
+	plt.legend()
+	fig.savefig(f'images/Accuracy on Perturbed Testing Data.jpg', bbox_inches='tight', dpi=150)
+
+	pgds=[PGD, PGD_Targeted]
+	epsilons=[0, .1, .2, .3, .45]
+	steps=40
+	alpha=0.01
+	p = True
+	for m in range(len(models)):
+		print()
+		print(f"Model: {runs[m][0][0]} - ϵ: {runs[m][0][1][0]} - steps: {runs[m][0][1][1]} - α: {runs[m][0][1][2]}")
+		fig_acc_test, ax_acc_test = plt.subplots()
+		ax_acc_test.set_title('Accuracy on Perturbed Testing Data')
+		for alg in pgds:
+			acc = []
+			for eps in epsilons:
+				ac = model_accuracy(models[m],pgd=alg, plot=p, pgd_per=[eps, steps, alpha])
+				print(f'40-step {alg.__name__} ϵ: {eps} - α: {alpha} \t {ac}')
+				acc.append(ac)
+			ax_acc_test.plot([n for n in epsilons], acc, label=f'{alg.__name__}')	
+			ax_acc_test.legend()
+		p = False
+		ax_acc_test.set(xlabel='ϵ', ylabel='Accuracy')
+		fig_acc_test.savefig(f'images/Model: {runs[m][0][0]} - ϵ: {runs[m][0][1][0]} - steps: {runs[m][0][1][1]} - α: {runs[m][0][1][2]}.jpg', bbox_inches='tight', dpi=150)
+
+	plt.close()
+
+	# plt.show()
 
 if __name__ == "__main__":
 	main()
 	print("End")
-
